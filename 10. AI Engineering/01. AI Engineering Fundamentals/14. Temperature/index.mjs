4530ee738c9c23c205b5e6f013223ee3e82ae9e6
iruobej <joshuairuobe@gmail.com>
2026-09-10 12:36:06 +0100

    Up to node js - the package json file

--- 10. AI Engineering/01. AI Engineering Fundamentals/14. Temperature/index.mjs
+++ 10. AI Engineering/01. AI Engineering Fundamentals/14. Temperature/index.mjs
@@ -42,7 +42,7 @@ async function fetchStockData() {
     loadingArea.style.display = 'flex'
     try {
         const stockData = await Promise.all(tickersArr.map(async (ticker) => {
-            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${process.env.POLYGON_API_KEY}`
+            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API_KEY}`
             const response = await fetch(url)
             const data = await response.text()
             const status = await response.status
@@ -80,6 +80,7 @@ async function fetchReport(data) {
 
     try {
         const openai = new OpenAI({
+            apiKey: import.meta.env.VITE_OPENAI_API_KEY,
             dangerouslyAllowBrowser: true
         })
         const response = await openai.chat.completions.create({
@@ -94,6 +95,7 @@ async function fetchReport(data) {
  **/
             model: 'gpt-4',
             messages: messages,
+            temperature: 1.1
         })
         renderReport(response.choices[0].message.content)
 
