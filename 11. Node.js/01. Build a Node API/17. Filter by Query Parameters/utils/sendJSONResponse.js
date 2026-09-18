dd1fa2d4ed71e5c36715b7b085dd5afc4d368ad7
iruobej <joshuairuobe@gmail.com>
2026-09-19 00:47:28 +0100

    finished build a node api

--- 11. Node.js/01. Build a Node API/17. Filter by Query Parameters/utils/sendJSONResponse.js
+++ 11. Node.js/01. Build a Node API/17. Filter by Query Parameters/utils/sendJSONResponse.js
@@ -1,5 +1,11 @@
 export const sendJSONResponse = (res, statusCode, payload) => {
     res.setHeader('Content-Type', 'application/json')
+    
+    // 'allow access from any domain using any port or protocol as long as they are using the GET method'
+    // needed for CORS
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+
     res.statusCode = statusCode
     res.end(JSON.stringify(payload))
 }
